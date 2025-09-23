e77a81d78944f84f812738070d6bc59a09bf205e
Aneesh Reddy <75503096+beabravedude@users.noreply.github.com>
2025-09-23 16:50:04 -0400

    Updated ASX to use default conditions in both explorer and active consolidations maps.

--- app/explorer/page.tsx
+++ app/explorer/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import AirspaceViewer from "@/components/Viewer/AirspaceViewer";
+import prisma from "@/lib/db";
 
 export default async function Page() {
 
+    const defaultConditions = await prisma.activeConsolidationsDefaultConditions.findFirst({
+        include: {
+            conditions: true,
+        },
+    });
+
     return (
-        <AirspaceViewer/>
+        <AirspaceViewer defaultConditions={defaultConditions?.conditions || []}/>
     );
 }
